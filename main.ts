1083113004052bdc86f4d77c0376df2e1c9058a1
Shaw54-eagle <286180124+Shaw54-eagle@users.noreply.github.com>
2026-08-31 12:22:09 -0400

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,36 @@
 input.onButtonPressed(Button.A, function () {
-	
+    music.setVolume(255)
+    music.play(music.stringPlayable("E C5 A E A F C5 A ", 160), music.PlaybackMode.InBackground)
+    basic.showLeds(`
+        # . . . .
+        . . # # .
+        . # . . .
+        . # . . .
+        . . # # .
+        `)
+    basic.showNumber(input.temperature())
 })
 input.onButtonPressed(Button.B, function () {
-	
+    music.setVolume(255)
+    music.play(music.stringPlayable("E C5 A E A F C5 A ", 160), music.PlaybackMode.InBackground)
+    basic.showLeds(`
+        # . . . .
+        . # # # .
+        . # . . .
+        . . # # .
+        . # . . .
+        `)
+    basic.showNumber(Math.convert(input.temperature(), UnitConversion.CelsiusToFahrenheit))
+})
+loops.everyInterval(3600000, function () {
+    music.setVolume(255)
+    music.play(music.stringPlayable("E C5 A E A F C5 A ", 160), music.PlaybackMode.InBackground)
+    basic.showLeds(`
+        # . . . .
+        . # # # .
+        . # . . .
+        . . # # .
+        . # . . .
+        `)
+    basic.showNumber(Math.convert(input.temperature(), UnitConversion.CelsiusToFahrenheit))
 })
